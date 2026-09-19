22ba8bd49d608fd7994efdaef52a9958dba978e1
Delis Guerra <98834151+DelisG@users.noreply.github.com>
2026-09-18 23:10:34 -0300

    fix: corrige consulta de CEP no ViaCEP

--- src/service/api.js
+++ src/service/api.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 export const obterEndereco = async(cep, setEndereco) => {
-    await axios.get(`https//viacep.com.br/ws/${cep}/json/`,)
-    .then(response => response.data())
+    await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+    .then(response => response.data)
     .then((endereco) => {
          setEndereco(endereco)
     })
